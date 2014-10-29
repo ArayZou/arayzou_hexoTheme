650a6dc5e387d21ed8717a6ef61dc57acf13a31e
arayzou <zrxldl@gmail.com>
2014-10-29 14:54:16 +0800

    随机定位所有内容格

--- source/about1/aboutzr.js
+++ source/about1/aboutzr.js
@@ -13,11 +13,29 @@ $(function(){
             width:(bodyWidth+1)*gridSize,
             height:(bodyHeight+1)*gridSize
         });
-        //填充空背景格
+        //填充空背景格,写入有效格坐标
+        var gridXY = [];
+        var gridNum = 0;
         for(var j=0;j<(bodyHeight+1);j++){
             for(var i=0;i<(bodyWidth+1);i++){
-                $gridBody.append('<b class="grid" id="g_'+(i+1)+'_'+(j+1)+'"></b>');
+                $gridBody.append('<b class="grid" id="g_'+i+'_'+j+'"></b>');
+                if(i!=bodyWidth&&j!=bodyHeight){
+                    gridXY[gridNum] = i+'.'+j;
+                    gridNum++
+                }
             }
+        }
+        //随机对有效坐标排序
+        var gridXYran=gridXY.sort(function(a,b){return Math.random()>=0.5?-1:1;});
+
+        var $gridU = $('.grid_u');
+        //定位所有内容格
+        for (var u = 0;u < $gridU.length;u++){
+            var uID = '#g_'+String(gridXYran[u]).split('.')[0]+'_'+String(gridXYran[u]).split('.')[1];
+            $gridU.eq(u).css({
+                left:$(uID).offset().left,
+                top:$(uID).offset().top
+            })
         }
 
         var Done = new Date();
